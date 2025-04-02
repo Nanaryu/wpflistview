9da5203f25d0b063970e119ea50ce58401f5a574
Nanaryu <112709452+Nanaryu@users.noreply.github.com>
2025-04-02 12:51:13 +0200

    Add files via upload

---
+++ wpflistview/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpflistview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string m_id { get; set; }
+        public string m_name { get; set; }
+        public string m_lname { get; set; }
+        public string m_p { get; set; }
+
+        class Item
+        {
+            string name, lname, pesel;
+            public Item(string name, string lname, string pesel)
+            {
+                this.name = name;
+                this.lname = lname;
+                this.pesel = pesel;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window = new Window1();
+            window.Show();
+        }
+    }
+}
---
+++ wpflistview/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpflistview
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        public string m_id { get; set; }
+        public string m_name { get; set; }
+        public string m_lname { get; set; }
+        public string m_p { get; set; }
+        Random rnd = new Random();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Owner as MainWindow;
+            mainWindow.ListView.Items.Add(new { m_id = rnd.Next(10000).ToString(), m_name = n1.Text, m_lname = n2.Text, m_p = n3.Text });
+            this.Close();
+        }
+    }
+}
